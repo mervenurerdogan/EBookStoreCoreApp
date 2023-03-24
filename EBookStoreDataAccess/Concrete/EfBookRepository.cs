a4436b6d1f20f172073b5e66555754d0f96afd6a
mervenurerdogan <merveeenur2@gmail.com>
2023-03-25 01:27:21 +0300

    Added Efrepository class

---
+++ EBookStoreDataAccess/Concrete/EfBookRepository.cs
@@ -0,0 +1,20 @@
+﻿using EBookStoreCore.Concrete.EntityFramework;
+using EBookStoreDataAccess.Abstract;
+using EBookStoreModel.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBookStoreDataAccess.Concrete
+{
+    public class EfBookRepository : EfEntityRepositoryBase<Book>, IBookRepository
+    {
+        public EfBookRepository(DbContext dbContext) : base(dbContext)
+        {
+            
+        }
+    }
+}
